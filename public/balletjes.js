1c2978f8fba165d5a94399a10e1e74550ed59324
issam <issam.buamran@coderclass.nl>
2017-10-20 10:00:00 +0200

    Alles weer naar het oude gezet

--- public/balletjes.js
+++ public/balletjes.js
@@ -5,16 +5,20 @@ var xSize = 600;
 var ySize = 200;
 var xBall = 21;
 var yBall = 21;
+var xBall2 = 21
 
 function setup(){
   createCanvas(xSize, ySize);
 }
 
 function draw(){
-  background(100);
+  background(255);
   fill(255, 220, 115);
   if (xBall > width - radius || xBall < radius){
       xSpeed = -xSpeed;
+      if (xspeed >= 40 || xspeed <= -40){
+        console.log("Groter dan 40");
+        xspeed = 10;
   }
   if (yBall > height - radius || yBall < radius){
       ySpeed = -ySpeed;
@@ -22,5 +26,4 @@ function draw(){
   xBall += xSpeed;
   yBall += ySpeed;
   ellipse(xBall, yBall, 2*radius, 2*radius);
-  }
 }
