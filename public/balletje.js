49fb14f6160b93ae259456bcccf871b24db0c512
issam <issam.buamran@coderclass.nl>
2017-11-02 10:04:17 +0100

    Een 2e bal gemaakt, maar hij staat stil

--- public/balletje.js
+++ public/balletje.js
@@ -1,10 +1,20 @@
 var radius = 10;
 var xSpeed = 5;
 var ySpeed = 3;
+var xSpeed2 = 5;
+var ySpeed2 = 3;
 var xSize = 600;
 var ySize = 200;
 var xBall = 21;
 var yBall = 21;
+var xBall2 = 21;
+var yBall2 = 21;
+
+
+var bal2 = {
+  x: 50,
+  y: 10
+};
 
 function setup(){
   createCanvas(xSize, ySize);
@@ -18,7 +28,16 @@ function draw(){
   if (yBall > height - radius || yBall < radius){
       ySpeed = -ySpeed;
   }
+  if (bal2.x > width - radius || bal2.x < radius){
+      xSpeed2 = -xSpeed2;
+      console.log("In de bal 2 x");
+  }
+  if (xBall2 > width - radius || xBall2 < radius){
+      xSpeed2 = -xSpeed2;
+      console.log("In de bal 2 y");
+  }
   xBall += xSpeed;
   yBall += ySpeed;
   ellipse(xBall, yBall, 2*radius, 2*radius);
+  ellipse(bal2.x, bal2.y, 2*radius, 2*radius);
 }
