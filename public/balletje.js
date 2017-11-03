feac0f805dc1c365db269e8d4d11686277898ccd
issam <issam.buamran@coderclass.nl>
2017-11-03 09:08:12 +0100

    Ervoor gezorgd dat de 2e balletje ook beweegd. Niet erg goed, maar in het begin doet hij het wel

--- public/balletje.js
+++ public/balletje.js
@@ -1,20 +1,14 @@
 var radius = 10;
-var xSpeed = 5;
+var xSpeed = 2;
 var ySpeed = 3;
-var xSpeed2 = 5;
-var ySpeed2 = 3;
 var xSize = 600;
 var ySize = 200;
 var xBall = 21;
 var yBall = 21;
-var xBall2 = 21;
-var yBall2 = 21;
-
-
-var bal2 = {
-  x: 50,
-  y: 10
-};
+var xSpeed2 = 5;
+var ySpeed2 = 3;
+var xBall2 = 50;
+var yBall2 = 50;
 
 function setup(){
   createCanvas(xSize, ySize);
@@ -24,20 +18,27 @@ function draw(){
   background(255);
   if (xBall > width - radius || xBall < radius){
       xSpeed = -xSpeed;
+      console.log("In x ball")
   }
   if (yBall > height - radius || yBall < radius){
       ySpeed = -ySpeed;
+      console.log("In y ball");
   }
-  if (bal2.x > width - radius || bal2.x < radius){
-      xSpeed2 = -xSpeed2;
-      console.log("In de bal 2 x");
-  }
-  if (xBall2 > width - radius || xBall2 < radius){
-      xSpeed2 = -xSpeed2;
-      console.log("In de bal 2 y");
-  }
+
   xBall += xSpeed;
   yBall += ySpeed;
   ellipse(xBall, yBall, 2*radius, 2*radius);
-  ellipse(bal2.x, bal2.y, 2*radius, 2*radius);
+
+  if (xBall2 > width - radius || xBall2 < radius){
+      xSpeed2 = -xSpeed2;
+      console.log("In de bal 2 x");
+  }
+  if (yBall2 > height - radius || yBall2 < radius){
+      xSpeed2 = -xSpeed2;
+      console.log("In de bal 2 y");
+  }
+
+  xBall2 += xSpeed2;
+  yBall2 += ySpeed2;
+  ellipse(xBall2, yBall2, 2*radius, 2*radius);
 }
